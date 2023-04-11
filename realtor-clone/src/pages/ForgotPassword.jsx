67c3c859b99d1e12a8327b052d16636bc5c3f59f
chiefkufre <samuelkufrewillie@gmail.com>
2023-04-11 04:55:31 +0100

    feat: complete forget-password page functionality

--- realtor-clone/src/pages/ForgotPassword.jsx
+++ realtor-clone/src/pages/ForgotPassword.jsx
@@ -1,9 +1,16 @@
 import { useState } from 'react'
 import { Link } from 'react-router-dom';
 import OAuth from '../components/OAuth';
+import {toast} from 'react-toastify';
+import { useNavigate } from 'react-router-dom';
 
+
+// firebase imports
+import {getAuth, sendPasswordResetEmail, confirmPasswordReset } from "firebase/auth";
 
 const ForgotPassword = () => {
+  const navigate = useNavigate();
+
   const [showPassword, setShowPassword] = useState(false);
 
   const [email, setEmail] = useState("");
@@ -11,6 +18,19 @@ const ForgotPassword = () => {
   const onChange = (e) => {
      setEmail(e.target.value);
   };
+
+  const passwordReset = async(e) => {
+    e.preventDefault();
+    try {
+      const auth = getAuth();
+      await sendPasswordResetEmail(auth, email);
+      toast.success("Reset password sent");
+    } catch (error) {
+      toast.error("unable to send password reset email");
+    }
+    
+
+  }
 
   return (
     <section>
@@ -22,7 +42,7 @@ const ForgotPassword = () => {
           />
         </div>
         <div className='w-full md:w-[67%] lg:w-[40%] lg:ml-20'>
-          <form action="" >
+          <form onSubmit={passwordReset} >
             <input type="email" id='email' value={email} onChange={onChange} placeholder='Email Address' className='mb-2 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out' />
 
             <div className='flex justify-between whitespace-nowrap text-sm sm:text-lg'>
@@ -30,11 +50,11 @@ const ForgotPassword = () => {
                 <Link to="/register" className='text-red-600 hover:text-red-900 transition duration-200 ease-in-out ml-1'>Register</Link>
               </p>
               <p>
-                <Link to="/forgot-password" className='text-blue-600 hover:text-blue-900 transition duration-200 ease-in-out'>Forgot Password</Link>
+                <Link to="/login" className='text-blue-600 hover:text-blue-900 transition duration-200 ease-in-out'>Login</Link>
               </p>
             </div>
             <div className='bg-blue-600 text-center py-2 rounded-2xl text-white text-sm font-medium hover:bg-blue-800 cursor-pointer transition duration-150 ease-in-out hover:shadow-lg'>
-              <input className='uppercase' type="button" value="Send Reset Password" />
+              <input className='uppercase' type="submit" value="Send Reset Password" />
             </div>
           </form>
           <div className='flex text-center my-4 before:border-t before:flex-1 before:border-gray-400 after:border-t after:flex-1 after:border-gray-400'>
